b2341322444636c48c20c27d89e0c5bffb1166d4
Emmanuel Antonio Beltre .S <60863456+emmanuelbeltre@users.noreply.github.com>
2022-08-24 11:21:04 -0400

    navigation witth fluroRouter implemented

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,3 +1,4 @@
+import 'package:bases_web/ui/views/counter_provider_view.dart';
 import 'package:bases_web/ui/views/counter_view.dart';
 import 'package:fluro/fluro.dart';
 
@@ -5,10 +6,23 @@ class Flurorouter {
   static final FluroRouter router = FluroRouter();
 
   static void configureRoutes() {
-    router.define('/', handler: _counterHandler);
+    router.define('/',
+        handler: _counterHandler, transitionType: TransitionType.fadeIn);
+    router.define('/stateful',
+        handler: _counterStatefulHandler,
+        transitionType: TransitionType.fadeIn);
+    router.define('/provider',
+        handler: _counterProviderHandler,
+        transitionType: TransitionType.fadeIn);
   }
 
 //Handlers o manejadores
   static Handler _counterHandler =
       new Handler(handlerFunc: (context, params) => CounterView());
+//Handlers o manejadores
+  static Handler _counterStatefulHandler =
+      new Handler(handlerFunc: (context, params) => CounterView());
+//Handlers o manejadores
+  static Handler _counterProviderHandler =
+      new Handler(handlerFunc: (context, params) => CounterProviderView());
 }
