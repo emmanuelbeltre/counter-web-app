a387e523342daecc62f0e16b18175bb945fa020e
Emmanuel Antonio Beltre .S <60863456+emmanuelbeltre@users.noreply.github.com>
2022-08-24 12:03:10 -0400

    404 view added to routes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,6 +1,8 @@
+import 'package:fluro/fluro.dart';
+
 import 'package:bases_web/ui/views/counter_provider_view.dart';
 import 'package:bases_web/ui/views/counter_view.dart';
-import 'package:fluro/fluro.dart';
+import 'package:bases_web/ui/views/view_404.dart';
 
 class Flurorouter {
   static final FluroRouter router = FluroRouter();
@@ -14,6 +16,8 @@ class Flurorouter {
     router.define('/provider',
         handler: _counterProviderHandler,
         transitionType: TransitionType.fadeIn);
+
+    router.notFoundHandler = _pageNotFoundHandler;
   }
 
 //Handlers o manejadores
@@ -25,4 +29,6 @@ class Flurorouter {
 //Handlers o manejadores
   static Handler _counterProviderHandler =
       new Handler(handlerFunc: (context, params) => CounterProviderView());
+  static Handler _pageNotFoundHandler =
+      new Handler(handlerFunc: (context, params) => View404());
 }
